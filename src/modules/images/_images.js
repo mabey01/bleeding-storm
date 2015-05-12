2a3d8bfe90871248e71e6d68049bdaa4a9903849
Maximilian <mabey01@gmail.com>
2015-05-12 16:39:21 +0200

    added interactive imageBackground

---
+++ src/modules/images/_images.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Maximilian on 12.05.2015.
+ */
+
+var bsImagesModule = angular.module('bsImages', [])
+    .constant("bsImages.FullBackgroundImages", [
+        //'fullBackground.jpg',
+        //'fullBackground2.jpg',
+        'fullBackground3.jpg',
+        //'fullBackground4.jpg',
+        //'fullBackground5.jpg',
+        'fullBackground6.jpg',
+        //'fullBackground7.jpg',
+        //'fullBackground8.jpg',
+        'fullBackground9.jpg',
+        'fullBackground10.jpg',
+        //'fullBackground11.jpg',
+        //'fullBackground12.jpg',
+        //'fullBackground13.jpg',
+        'fullBackground14.jpg',
+        //'fullBackground15.jpg',
+        'fullBackground16.jpg',
+        'fullBackground17.jpg',
+        'fullBackground18.jpg',
+        'fullBackground19.jpg',
+        'fullBackground20.jpg'
+    ]);
---
+++ src/modules/images/directives/bsBackgroundImage.js
@@ -0,0 +1,87 @@
+/**
+ * Created by Maximilian on 12.05.2015.
+ */
+
+bsImagesModule.directive('bsBackgroundImage', ["bsImages.FullBackgroundImages", function(fullBackgroundImages) {
+
+    var loadImages = function(images, dir = '/') {
+        return new Promise((resolve, reject) => {
+            let loadedImages = [];
+            let finalLenght = images.length;
+            let onLoad = (image) => {
+                loadedImages.push(image);
+                if (loadedImages.length === finalLenght) resolve(loadedImages);
+            };
+
+            let onError = (image) => {
+                finalLenght--;
+                if (loadedImages.length === finalLenght) resolve(loadedImages);
+            };
+
+            images.forEach((imageString) => {
+                let newImage = new Image();
+                newImage.onload = () => onLoad(newImage);
+                newImage.onerror = () => onError(newImage);
+                newImage.src = dir + imageString;
+            })
+        })
+    };
+
+    return {
+        restrict : 'A',
+        link : function(scope, element, attrs) {
+            const INTERVAL_TIME = 10000;
+            let imagesDir = 'images/';
+            let loadedImages = [];
+            let rotatingInterval = null;
+            let index = -1;
+
+            let imageContainer = angular.element('<div class="bsBackroundImage"></div>');
+            element.prepend(imageContainer);
+
+            let showNextImage = function() {
+                try {
+                    loadedImages[index].className = 'hidden';
+                } catch(e) {}
+
+                if (index + 1 >= loadedImages.length) index = 0; else index++;
+                loadedImages[index].className = 'visible';
+            };
+
+            let rotateImages = () => {
+                rotatingInterval = setInterval(showNextImage, INTERVAL_TIME);
+            };
+
+            var keydown = false;
+            element.on("keydown", function (event) {
+                if (event.keyCode === 39 && event.shiftKey) keydown = true;
+            });
+
+            element.on("keyup", function (event) {
+                if (keydown) {
+                    clearInterval(rotatingInterval);
+                    showNextImage();
+                    //rotateImages();
+                    keydown = false;
+                }
+            });
+
+
+            let startingTime = Date.now();
+            loadImages(fullBackgroundImages, imagesDir).then(
+                function(_loadedImages) {
+                    loadedImages = _loadedImages;
+                    loadedImages.forEach((image) => {
+                        image.className = 'hidden';
+                        imageContainer.append(image);
+                    });
+
+                    if ((Date.now() - startingTime) > 60*1000) {
+                        showNextImage();
+                    }
+                    rotateImages();
+                }
+            );
+        }
+    }
+}]);
